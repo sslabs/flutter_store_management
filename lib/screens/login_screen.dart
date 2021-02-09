541f183b4783ac4e14b266d3d89f02ab2503c7e8
Plinio Sandes dos Santos <psandes@conder.ba.gov.br>
2021-02-09 17:01:42 -0300

    Add the login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:store_management/widgets/input_field.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -9,21 +10,47 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[850],
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Icon(
-                  Icons.store_mall_directory,
-                color: Colors.pinkAccent,
-                  size: 160,
-              )
-            ],
-          ),
-        ),
-      )
-    );
+        backgroundColor: Colors.grey[850],
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(),
+            SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Icon(
+                      Icons.store_mall_directory,
+                      color: Colors.pinkAccent,
+                      size: 160,
+                    ),
+                    InputField(
+                        icon: Icons.person_outline,
+                        hint: 'Usuário',
+                        obscure: false
+                    ),
+                    InputField(
+                        icon: Icons.lock_outline,
+                        hint: 'Senha',
+                        obscure: true
+                    ),
+                    SizedBox(height: 32),
+                    SizedBox(
+                      height: 50,
+                      child: RaisedButton(
+                        color: Colors.pinkAccent,
+                        child: Text('Entrar'),
+                        onPressed: () {},
+                        textColor: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
